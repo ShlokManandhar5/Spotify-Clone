4f8c931256b6396cc1bdb49584b24f62e59dcab7
ShlokManandhar5 <shlokmanandhar5@gmail.com>
2025-01-02 23:12:32 +0545

    this the new commit

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+let currentSong= new Audio();
+
 async function getSongs() {
     let a = await fetch('http://127.0.0.1:5500/songs/')
     let response = await a.text()
@@ -15,19 +18,27 @@ async function getSongs() {
     return songs;
 
 }
+
+const playMusic = (track) => {
+    currentSong.src = "/songs/" + track;  
+    currentSong.play();
+    play.src = "svgs/pause.svg";
+    document.querySelector(".songInfo").innerHTML = track;
+    document.querySelector(".songTime").innerHTML = "00:00 / 00:00";
+}
+
+
 async function main() {
 
     //get the list of all the songs 
     let songs = await getSongs();
     console.log(songs);
 
+    //Show all the songs in the playlist 
     let songUL = document.querySelector('.songList').getElementsByTagName('ul')[0];
     for (const song of songs) {
-        let songName = song.replaceAll("%20", " ").replaceAll("%2C", " ").replaceAll("%26", " ").replaceAll(".mp3", "");
-
-        // Split by multiple delimiters (' - ', 'll', '(', etc.) and take the first part
+        let songName = song.replaceAll("%20", " ").replaceAll("%2C", " ").replaceAll("%26", " ");
         let displayName = songName.split(/ - | ll | \( /)[0].trim();
-
         songUL.innerHTML = songUL.innerHTML + `<li>
         <img class="invert" src="svgs/music.svg" alt="">
         <div class="info">
@@ -41,14 +52,24 @@ async function main() {
     </li>`;
     }
 
-    //play the first song
-    var audio = new Audio(songs[0]);
-    audio.play();
-
-    audio.addEventListener('ontimeupdate', () => {
-        console.log(audio.duration, audio.currentSrc, audio.currentTime);
-        //The duration variable now holds the duration of the song in seconds
+    //attact an event listener to each songs
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener('click', element => {
+            console.log(e.querySelector(".info").firstElementChild.innerHTML);
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML);
+        })
     })
+
+    //attact an event listener to play, next and previous buttons
+    play.addEventListener('click', () => {
+        if (currentSong.paused) {
+            currentSong.play();
+            play.src = "svgs/pause.svg";
+        } else {
+            currentSong.pause();
+            play.src = "svgs/play.svg";
+        }
+    });
 }
 
 main();
