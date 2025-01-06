e11fbeb0fa5dc50aaaa1ea559070e9795f2a6681
ShlokManandhar5 <shlokmanandhar5@gmail.com>
2025-01-06 18:11:03 +0545

    new update

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 
 let currentSong= new Audio();
 
+function secondsToMinutesSeconds(seconds) {
+    if (isNaN(seconds) || seconds < 0) {
+        return "00:00";
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+
+
 async function getSongs() {
     let a = await fetch('http://127.0.0.1:5500/songs/')
     let response = await a.text()
@@ -69,6 +84,12 @@ async function main() {
             currentSong.pause();
             play.src = "svgs/play.svg";
         }
+    });
+
+    // listen for timeupdate event
+    currentSong.addEventListener('timeupdate', () => {
+        console.log(currentSong.currentTime, currentSong.duration);
+        document.querySelector(".songTime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`;
     });
 }
 
